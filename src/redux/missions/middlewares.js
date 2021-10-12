0318d64ad630d2677d4103f26702f10c5b5897ce
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2021-10-12 21:51:27 +0500

    Apply middlewares

---
+++ src/redux/missions/middlewares.js
@@ -0,0 +1,12 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import spaceReducer from './missions';
+
+const reducer = combineReducers({
+  spaceReducer,
+});
+
+const store = createStore(reducer, applyMiddleware(thunk, logger));
+
+export default store;
