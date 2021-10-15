954a397e6e8828130bd26602293ab5c10ececba0
Kossi-stack <81444338+Kossi-stack@users.noreply.github.com>
2021-10-15 10:03:49 -0700

    Add reducer to config file

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -3,9 +3,10 @@ import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import missionReducer from './missions/missions';
 import rocketReducer from './rockets/rockets';
+import dragonReducer from './dragons/dragons';
 
 const reducer = combineReducers({
-  missionReducer, rocketReducer,
+  missionReducer, rocketReducer, dragonReducer,
 });
 
 const store = createStore(reducer, applyMiddleware(thunk, logger));
