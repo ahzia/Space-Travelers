ac067c73376effbccd4e3b0c06d4b5eec355f93a
Ahmad Zia <ah.ziayosfi@gmail.com>
2021-10-16 02:21:56 +0430

    displaying reserved rockets on my profile

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,9 +1,15 @@
+import MyRockets from './MyRockets';
 import ProfileMissions from './ProfileMission';
 
 const MyProfile = () => (
-  <div>
-    <div>
-      <ProfileMissions />
+  <div className="container">
+    <div className="row">
+      <div className="col-md-3">
+        <ProfileMissions />
+      </div>
+      <div className="col-md-3">
+        <MyRockets />
+      </div>
     </div>
   </div>
 );
