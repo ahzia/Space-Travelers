bed91e6cf5333f26c6a7323e1f424491f374cfd1
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2021-10-16 18:24:21 +0500

    Make code much readable
    I also left some comments I have tried to use this pattern but it is showing me some errors.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux'; // useSelectors
 import { fetchPostsRequestRockets } from './redux/rockets/rockets';
+import { fetchPostsRequestMissions } from './redux/missions/missions';
 import store from './redux/configureStore';
 import NavBar from './components/NavBar';
 import Missions from './components/Mission';
@@ -12,15 +13,21 @@ import './App.css';
 
 const App = () => {
   const [rockets, setRockets] = useState([]);
+  const [missions, setMissons] = useState([]);
   const dispatch = useDispatch();
 
   useEffect(() => {
+    dispatch(fetchPostsRequestMissions());
     dispatch(fetchPostsRequestRockets());
   }, []);
+
+  // const missions = useSelector((state) => state.spaceReducer.missions);
+  // const rockets = useSelector((state) => state.spaceReducer.rockets);
 
   useEffect(() => {
     store.subscribe(() => {
       setRockets(store.getState().rocketReducer.rockets);
+      setMissons(store.getState().missionReducer.missions);
     });
   });
   return (
@@ -32,7 +39,7 @@ const App = () => {
         <main>
           <Switch>
             <Route path="/Mission">
-              <Missions />
+              <Missions missions={missions} />
             </Route>
             <Route path="/Dragons">
               <Dragons />
